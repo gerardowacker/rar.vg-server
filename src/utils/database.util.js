0e53aa556165b4a23aacd14bb65c6aadbeb968e4
juanvecchio <vecchiojuanignacio@gmail.com>
2023-05-24 23:21:49 -0300

    Created database util

--- src/utils/database.util.js
+++ src/utils/database.util.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2')
+const config = require('./config.util')
 
 const pool = mysql.createPool({
-  host:,
-  user:,
-  database:,
-  password:,
+    host: config("DB_HOST"),
+    user: config("DB_USER"),
+    database: config("DB_DATABASE"),
+    password: config("DB_PASSWORD"),
 })
 
 module.exports = pool.promise()
