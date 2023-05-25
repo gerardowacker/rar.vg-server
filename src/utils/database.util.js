e23c589cc6fdfc7f54d7e4b706de0fc0cf6f62d4
juanvecchio <vecchiojuanignacio@gmail.com>
2023-05-25 01:17:17 -0300

    Added port into `database.util.js`

--- src/utils/database.util.js
+++ src/utils/database.util.js
@@ -3,6 +3,7 @@ const config = require('./config.util')
 
 const pool = mysql.createPool({
     host: config("DB_HOST"),
+    port: config("DB_PORT"),
     user: config("DB_USER"),
     database: config("DB_DATABASE"),
     password: config("DB_PASSWORD"),
