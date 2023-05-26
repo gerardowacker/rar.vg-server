5e82efdc8f3f66a43c0db96acdceb2817f83ce13
juanvecchio <vecchiojuanignacio@gmail.com>
2023-05-26 01:51:08 -0300

    Added and modified methods in `user.model.js`
    Co-authored-by: Gerardo Wacker <ge@rar.vg>

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,29 +2,83 @@ const db = require('../utils/database.util.js')
 
 module.exports = class User
 {
-    constructor(id, username, password, displayName, email, creationDate, dateOfBirth, links, components)
+  #id; #username; #password; #displayName; #email; #creationDate; #dateOfBirth; #sociallinks; #components; #isSQLSynced;  
+  constructor(id, username, password, displayName, email, creationDate, dateOfBirth, sociallinks, components)
     {
-        this.id = id
-        this.username = username
-        this.password = password
-        this.displayName = displayName
-        this.creationDate = creationDate
-        this.dateOfBirth = dateOfBirth
-        this.links = links
-        this.email = email
-        this.components = components
+        this.#id = id
+        this.#username = username
+        this.#password = password
+        this.#displayName = displayName
+        this.#creationDate = creationDate
+        this.#dateOfBirth = dateOfBirth
+        this.#sociallinks = sociallinks
+        this.#email = email
+        this.#components = components
+        this.#isSQLSynced = false
     }
 
-    static find(matches)
+    static find(...matches)
     {
-        let queryKeys = Object.keys(matches)
-        let argument = 'SELECT id, username, password, displayName, creationDate, dateOfBirth, components, sociallinks FROM Users WHERE'
+      let argument = 'SELECT id, username, password, displayName, creationDate, dateOfBirth, components, sociallinks FROM Users WHERE'  
+      for (let i = 0; i < matches.length; i++){
+          let match = matches[i]
+          let queryKeys = Object.keys(match)
+          let subargument = (i === 0 ? ' (' : ' OR (')
+          for (let j = 0; j < queryKeys.length; j++)
+          {
+            subargument = subargument + ((j === 0 ? ' ' : ' AND ') + queryKeys[j] + ' = \'' + match[queryKeys] + '\'')
+          }
+          subargument = subargument + ")"
+          argument = argument + subargument
+      }
+        return db.execute(argument)
+    }
 
-        for (let i = 0; i < queryKeys.length; i++)
+    static async findOne(matches){
+      const [users] = await User.find(matches)
+      const user = users[0]
+      
+      const u = new User(user.id, user.username, user.password, user.displayName, user.email, user.creationDate, user.dateOfBirth, user.sociallinks, user.components)
+      u.setSQLSynced(true) 
+
+      return u
+    }
+
+    update(values){
+      return new Promise(res => {
+        let valueKeys = Object.keys(values)
+        let argument = 'UPDATE Users SET'
+
+        for (let i = 0; i < valueKeys.length; i++)
         {
-            argument = argument + ((i === 0 ? ' ' : ' AND ') + queryKeys[i] + ' = \'' + matches[queryKeys] + '\'')
+            argument = argument + ((i === 0 ? ' ' : ', ') + valueKeys[i] + ' = \'' + values[valueKeys] + '\'')
         }
 
-        return db.execute(argument)
+        argument = argument + 'WHERE id = ' + this.#id
+        
+        db.execute(argument).then(result =>{
+          this.#username = values['username'] || this.#username
+          this.#password = values['password'] || this.#password
+          this.#displayName = values['displayName'] || this.#displayName
+          this.#email = values['email'] || this.#email
+          this.#creationDate = values['creationDate'] || this.#creationDate
+          this.#dateOfBirth = values['dateOfBirth'] || this.#dateOfBirth
+          this.#sociallinks = values['sociallinks'] || this.#sociallinks
+          this.#components = values['components'] || this.#components
+
+          res(result)
+        })
+      })
+    }
+
+    setSQLSynced(value){
+      this.#isSQLSynced = value 
+    }
+
+    insert(){
+      if(this.#isSQLSynced) return
+      return db.execute("INSERT INTO Users (username, password, displayName, creationDate, dateOfBirth, socialLinks, email, components) VALUES ('" +
+            this.#username + "', '" + this.#password + "', '" + this.#displayName + "', '" + this.#creationDate + "', '" + this.#dateOfBirth + "', '" +
+            JSON.stringify(this.#sociallinks) + "', '" + this.#email + "', '" + JSON.stringify(this.#components) + "')")
     }
 }
