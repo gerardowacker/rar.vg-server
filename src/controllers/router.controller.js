ba6d3a53b0ca339afee6f676632804af5fa70a2b
Gerardo Wacker <ge@rar.vg>
2023-05-29 20:03:22 -0300

    Added HTTP status code sending within router

--- src/controllers/router.controller.js
+++ src/controllers/router.controller.js
@@ -9,7 +9,7 @@ class RouterController
     {
         this.sessionController = new SessionController()
         this.userController = new UserController(this.sessionController)
-        this.fileController = new FileController()
+        this.fileController = new FileController(this.sessionController)
     }
 
     // Creates the server.
@@ -21,13 +21,13 @@ class RouterController
             const router = express.Router()
             // Configure every route, then resolve the promise.
             router.get('/', (req, res) => res.send("la curiosidad mató al gato"))
-            router.get('/profile/:user', (req, res) => this.userController.getProfile(req.params.user).then(result => res.send(result)))
-            router.post('/files/upload', (req, res) => this.fileController.upload(req.files, req.body.session, req.body.avatar).then(result => res.status(result.status).send(result.content)))
-            router.post('/register', (req, res) => this.userController.register(req.body).then(result => res.send(result)))
-            router.post('/login', (req, res) => this.userController.login(req.body).then(result => res.send(result)))
-            router.post('/validate', (req, res) => this.sessionController.validate(req.body.token, req.body.clientToken).then(result => res.send(result)))
-            router.post('/updateComponents', (req, res) => this.userController.updateComponents(req.body.token, req.body.clientToken, req.body.components).then(result => res.send(result)))
-            router.post('/updateLinks', (req, res) => this.userController.updateLinks(req.body.token, req.body.clientToken, req.body.links).then(result => res.send(result)))
+            router.get('/profile/:user', (req, res) => this.userController.getProfile(req.params.user).then(result => res.status(result.status).send(result.content)))
+            router.post('/files/upload', (req, res) => this.fileController.upload(req.files, req.body.token, req.body.clientToken, req.body.avatar === '1').then(result => res.status(result.status).send(result.content)))
+            router.post('/register', (req, res) => this.userController.register(req.body).then(result => res.status(result.status).send(result.content)))
+            router.post('/login', (req, res) => this.userController.login(req.body).then(result => res.status(result.status).send(result.content)))
+            router.post('/validate', (req, res) => this.sessionController.validate(req.body.token, req.body.clientToken).then(result => res.status(result.status).send(result.content)))
+            router.post('/updateComponents', (req, res) => this.userController.updateComponents(req.body.token, req.body.clientToken, req.body.components).then(result => res.status(result.status).send(result.content)))
+            router.post('/updateLinks', (req, res) => this.userController.updateLinks(req.body.token, req.body.clientToken, req.body.links).then(result => res.status(result.status).send(result.content)))
             res(router)
         })
     }
