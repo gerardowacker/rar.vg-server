a46bd2314c06a6e53b4a4f4c97d0c072961c0aad
Gerardo Wacker <ge@rar.vg>
2023-05-29 20:03:04 -0300

    Add session validation within `FileController`

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -1,50 +1,61 @@
+const path = require('path')
+const root = path.normalize(path.join(path.dirname(require.main.filename), '..'))
+
 module.exports = class FileController
 {
-    upload(files, session, avatar)
+    constructor(sessionController)
     {
-        return new Promise(resolve =>
+        this.sessionController = sessionController
+    }
+
+    upload(files, token, clientToken, avatar)
+    {
+        return new Promise(res =>
         {
             if (!files || Object.keys(files).length === 0)
             {
-                resolve({
+                res({
                     status: 400,
                     content: "No files were uploaded."
                 })
             }
 
-            // TODO: Check for session token in here. Should be able to work for now, but needs to
-            // include everything authentication when it's implemented.
+            this.sessionController.validate(token, clientToken).then(sessionResult =>
+            {
+                if (sessionResult.status !== 200)
+                    return res(sessionResult)
 
-            const file = files.theFile;
-            const fileExtension = file.split(".").pop()
-            const newFileName = this.#generateString() + '.' + fileExtension
-            let uploadPath;
-            if (fileExtension !== "png" && fileExtension !== "jpg" && fileExtension !== "webp" && fileExtension !== "pdf")
-                resolve({
-                    status: 400,
-                    content: "File format is not allowed."
-                })
-            if (avatar)
-            {
-                const id = 0 // Will get everything done once session parsing is implemented.
-                uploadPath = __dirname + '/public/avatars/' + id + '.' + fileExtension;
-            }
-            else
-            {
-                uploadPath = __dirname + '/public/userfiles/' + newFileName;
-            }
-
-            file.mv(uploadPath, err =>
-            {
-                if (err)
-                    resolve({
-                        status: 500,
-                        content: err
+                const file = files.theFile;
+                const fileExtension = file.name.split(".").pop()
+                const newFileName = this.#generateString() + '.' + fileExtension
+                let uploadPath;
+                if (fileExtension !== "png" && fileExtension !== "jpg" && fileExtension !== "webp" && fileExtension !== "pdf")
+                    return res({
+                        status: 400,
+                        content: "File format is not allowed."
                     })
+                if (avatar)
+                {
+                    const id = sessionResult.content.id
+                    uploadPath = root + '/public/avatars/' + id + '.png';
+                }
+                else
+                {
+                    uploadPath = root + '/public/userfiles/' + newFileName;
+                }
 
-                resolve({
-                    status: 200,
-                    content: newFileName
+                file.mv(uploadPath, err =>
+                {
+                    if (err)
+                        return res({
+                            status: 500,
+                            content: err
+                        })
+
+                    res({
+                        status: 200,
+                        content: newFileName
+                    })
                 })
             })
         })
