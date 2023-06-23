89af1282e13f7daef8585360765c0575bbdc36c4
Gerardo Wacker <ge@rar.vg>
2023-06-23 11:30:27 -0300

    Added an initial basic instance of `EmailController`

---
+++ src/controllers/email.controller.js
@@ -0,0 +1,38 @@
+const nodemailer = require('nodemailer')
+
+const config = require('../../config/config.json')
+
+module.exports = class EmailController
+{
+    connect()
+    {
+        return new Promise(res =>
+        {
+            if (!this.transporter)
+                this.transporter = nodemailer.createTransport({
+                    host: config.MAIL_HOST,
+                    port: 465,
+                    secure: true,
+                    auth: {
+                        user: config.MAIL_ADDRESS,
+                        pass: config.MAIL_PASSWORD
+                    }
+                })
+            res(this)
+        })
+    }
+
+    send(subject, body, recipient)
+    {
+        return new Promise(async res =>
+        {
+            let email = await this.transporter.sendMail({
+                from: `"${config.MAIL_ALIAS}" <${config.MAIL_ADDRESS}>`,
+                to: recipient,
+                subject: subject,
+                text: body
+            })
+            return res({success: true, content: email})
+        })
+    }
+}
