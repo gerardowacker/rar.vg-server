b8fb04a82439061ec2ec451466999afc520f2449
Gerardo Wacker <ge@rar.vg>
2023-09-17 20:08:05 -0300

    Added a ninth `?` into SQL query

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -133,7 +133,7 @@ module.exports = class User
     insert()
     {
         if (this.#isSQLSynced) return
-        return db.execute("INSERT INTO Users (username, password, displayName, creationDate, dateOfBirth, socialLinks, email, components, profileDesign) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+        return db.execute("INSERT INTO Users (username, password, displayName, creationDate, dateOfBirth, socialLinks, email, components, profileDesign) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
             [this.username, this.password, this.displayName, this.creationDate, this.dateOfBirth, JSON.stringify(this.sociallinks), this.email, JSON.stringify(this.components)], JSON.stringify(this.profileDesign))
     }
 }
